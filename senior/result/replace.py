5468c23717ebc0879ecf71bb58611c6997ca2d11
Hope <tembhum.ch@gmail.com>
2020-06-02 22:04:35 +0700

    Lucy

---
+++ senior/result/replace.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+dataset = pd.read_csv('monkeyresult.csv')
+def filter_function(dataframe):
+    states=[]
+    for index, row in dataset.iterrows():
+        string = str(row[1])+str(row[2])
+        if string not in states:
+            states.append(string)
+    return states
+
+filtered_states = filter_function(dataset)
+print("length :" + str(len(filtered_states)))
+print("states :" + str(filtered_states))
